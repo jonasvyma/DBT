e9bc325d2568a944b83537cf357a6771e501a135
Jonas Vyshniauskas <jonasvyma@gmail.com>
2025-09-10 16:25:52 +0200

    j

---
+++ models/prep/prep_airports.sql
@@ -0,0 +1,19 @@
+with airports_reorder as (
+select
+	region,
+	country,
+	faa,
+	name,
+	lat,
+	lon,
+	alt,
+	tz,
+	dst,
+	city
+from
+	{{ref('staging_airports')}}
+)
+select
+	*
+from
+	airports_reorder;
